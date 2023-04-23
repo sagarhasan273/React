f9fa8fd32ef1b2a6eabb6605675cfcb142179566
sagarhasan273 <sagarhasan273@gmail.com>
2023-04-24 00:10:05 +0600

    index.js edit

--- src/index.js
+++ src/index.js
@@ -12,10 +12,17 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import reportWebVitals from './reportWebVitals';
 
 function Start() {
-  return <App />;
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
 createRoot(document.getElementById('root')).render(
   <Start />,
 );
+
+reportWebVitals();
