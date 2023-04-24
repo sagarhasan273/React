e3fea1cf05a66392f298d061d7968123ecda4634
sagarhasan273 <sagarhasan273@gmail.com>
2023-04-25 00:35:30 +0600

    edit index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-/* eslint-disable react/no-access-state-in-setstate */
-/* eslint-disable react/button-has-type */
-/* eslint-disable eqeqeq */
-/* eslint-disable radix */
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable react/sort-comp */
-/* eslint-disable react/no-unused-state */
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/prefer-stateless-function */
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
@@ -21,6 +11,7 @@ function Start() {
     </React.StrictMode>
   );
 }
+
 createRoot(document.getElementById('root')).render(
   <Start />,
 );
