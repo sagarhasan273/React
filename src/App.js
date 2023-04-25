a1eb4eb331c6be8b67308155d21a5a12c93f3727
sagarhasan273 <sagarhasan273@gmail.com>
2023-04-25 10:08:03 +0600

    use variables to store elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-/* eslint-disable react/jsx-boolean-value */
-function UserGreeting() {
-  return <h1>Welcome Back!</h1>;
-}
-
-function GuestGreeting() {
-  return <h1>Please, Sign up!</h1>;
-}
-
-function Greeting(props) {
-  const { isLoggedIn } = props;
-  if (isLoggedIn) {
-    return <UserGreeting />;
-  }
-  return <GuestGreeting />;
-}
+/* eslint-disable import/no-cycle */
+import LoginControl from './components/LoginControl';
 
 function App() {
   return (
-    <Greeting isLoggedIn={true} />
+    <LoginControl />
   );
 }
 
