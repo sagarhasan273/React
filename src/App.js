7bd07cc29624f324fc18d80b286a8c0be5eef6b3
sagarhasan273 <sagarhasan273@gmail.com>
2023-04-25 09:16:54 +0600

    Functional Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import Clock from './Components/Clock';
+/* eslint-disable react/jsx-boolean-value */
+function UserGreeting() {
+  return <h1>Welcome Back!</h1>;
+}
+
+function GuestGreeting() {
+  return <h1>Please, Sign up!</h1>;
+}
+
+function Greeting(props) {
+  const { isLoggedIn } = props;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
 
 function App() {
-  console.log('App component rendered');
   return (
-    <Clock />
+    <Greeting isLoggedIn={true} />
   );
 }
 
