f6de810467aa6c705f6afde1cfed35b27c470b41
sagarhasan273 <sagarhasan273@gmail.com>
2023-09-12 09:05:24 +0600

    node position successful

--- src/components/Nodes.js
+++ src/components/Nodes.js
@@ -1,41 +1,37 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-expressions */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/no-array-index-key */
 import { useEffect, useState } from 'react';
 
-/* eslint-disable react/destructuring-assignment */
 function Nodes() {
     const [values, setValue] = useState([]);
     const positions = [];
-    const depthPos = { 4: 0 };
-    const num = 4;
+    const mx = [0, 20];
+    const num = 6;
 
     function fb(n, d) {
         if (n === 1 || n === 0) {
-            depthPos[d] += 55;
-            positions.push([depthPos[d], d * 50, n]);
-            return [depthPos[d], n, depthPos[d]];
+          positions.push([mx[0] + 40, d * 40, n]);
+          mx[0] += 40;
+          mx[1] = Math.max(mx[1], d * 40);
+          return mx[0];
         }
         const l = fb(n - 1, d + 1);
         const r = fb(n - 2, d + 1);
-        depthPos[d] = (l[0] + r[0]) / 2;
-        positions.push([depthPos[d], d * 50, n]);
-        return [depthPos[d], n, Math.max(l[2], r[2], depthPos[d])];
+        positions.push([(l + r) / 2, d * 40, n]);
+        return (l + r) / 2;
     }
 
-    const results = fb(num, 1);
-
+    fb(num, 1);
     useEffect(() => {
-        setValue(positions);
+      setValue(positions);
     }, []);
 
     return (
       <div style={{ width: '100%', height: '98vh', backgroundColor: 'yellow' }}>
-        <svg viewBox={`0 0 ${results[2] + 55} ${(num + 1) * 60}`}>
+        <svg viewBox={`0 0 ${mx[0] + 40} ${mx[1] + 30}`} style={{ width: '100%', height: '98vh', backgroundColor: 'yellow' }}>
           {values.map((value, index) => (
             <g key={index}>
-              <circle cx={value[0]} cy={value[1]} r="15" stroke="black" fill="none" strokeWidth={2} />
+              <circle cx={value[0]} cy={value[1]} r="10" stroke="black" fill="none" strokeWidth={1} />
               <text
                 x={value[0]}
                 y={value[1]}
