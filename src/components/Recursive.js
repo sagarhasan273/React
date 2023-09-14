08985aa31ee8cbddfb9a8e2127f5325aef3eda68
sagarhasan273 <sagarhasan273@gmail.com>
2023-09-14 17:00:50 +0600

    edges able traverse well

--- src/components/Recursive.js
+++ src/components/Recursive.js
@@ -39,31 +39,44 @@ export default function Recursive() {
     }, []);
 
     const handleChangePlus = () => {
+      if (range === nodeArray) return;
         setRange((prev) => prev + 1);
     };
     const handleChangeMinus = () => {
+      if (range === 0) return;
         setRange((prev) => prev - 1);
     };
 
+    const edgesList = new Map();
+    for (let index = 1; index < range; index += 1) {
+      const x1 = nodes[nodeArray[index - 1]][0];
+      const x2 = nodes[nodeArray[index - 0]][0];
+      const y1 = nodes[nodeArray[index - 1]][1];
+      const y2 = nodes[nodeArray[index - 0]][1];
+      const edgeNumString = (x1 + y1 > x2 + y2) ? `${x1 + y1}${x2 + y2}` : `${x2 + y2}${x1 + y1}`;
+      edgesList.set(edgeNumString, [x1, x2, y1, y2, index]);
+    }
+    console.log(edgesList);
+    const tempEdges = [];
+    edgesList.forEach((value, key) => {
+      tempEdges.push(
+        <LinkedLine
+          key={key + value[4]}
+          xy={key}
+          x1={value[0]}
+          x2={value[1]}
+          y1={value[2]}
+          y2={value[3]}
+          index={value[4]}
+        />,
+      );
+    });
     return (
       <div style={{ width: '100%', height: '90vh', backgroundColor: 'yellow' }}>
         <svg viewBox={`0 0 ${mx[0] + 30} ${mx[1] + 30}`} style={{ width: '100%', height: '100%', backgroundColor: 'yellow' }}>
           {nodeArray.slice(0, range).map((value, index) => (
-            <>
-              <Node key={index} value={value} nodes={nodes} nodeArray={nodeArray} />
-              {index >= 1 && index < nodeArray.length ? (
-                <LinkedLine
-                  key={index + 10000}
-                  x1={nodes[nodeArray[index - 1]][0]}
-                  x2={nodes[nodeArray[index - 0]][0]}
-                  y1={nodes[nodeArray[index - 1]][1]}
-                  y2={nodes[nodeArray[index - 0]][1]}
-                  index={index}
-                />
-) : null}
-            </>
-            ))}
-
+            <Node key={index} value={value} nodes={nodes} nodeArray={nodeArray} />))}
+          {tempEdges}
         </svg>
         <button type="button" name={1} onClick={handleChangePlus}>+</button>
         <button type="button" name={0} onClick={handleChangeMinus}>-</button>
